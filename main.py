8cf92b41f065ab4b0ad3f3a024eee9d0b6556104
Jeremy Grecu <jeremy.grecu@sap.com>
2025-12-15 17:28:35 +0100

    Add print report in main, add feature to extract char: num dictionary and sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from webbrowser import get
+from stats import get_num_words, get_chars, get_chars_list
 
 
 def get_book_text(path_to_file):
@@ -8,9 +9,21 @@ def get_book_text(path_to_file):
 
 
 def main():
-    file_contents = get_book_text("./books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    file_contents = get_book_text(book_path)
     num_words = get_num_words(file_contents)
+    chars_dict = get_chars(file_contents)
+    chars_list = get_chars_list(chars_dict)
+    print("=========== BOOKBOT ============")
+    print(f"Analyzing {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
+    for item in chars_list:
+        if item['char'].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def get_num_words(contents):
     words = contents.split()
     return len(words)
+
+
+def get_chars(contents):
+    chars = {}
+
+    for c in contents.lower():
+        chars[c] = chars.get(c, 0) + 1
+
+    return chars
+
+
+def get_chars_list(chars_dict):
+    chars = []
+
+    for key, val in chars_dict.items():
+        chars.append({"char": key, "num": val})
+
+    chars.sort(key=sort_on, reverse=True)
+
+    return chars
+
+
+def sort_on(items):
+    return items["num"]
