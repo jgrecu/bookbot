47c01b46b1e15610de084a5cd93233d8c066f40e
Jeremy Grecu <jeremy.grecu@sap.com>
2025-12-15 17:01:55 +0100

    Refactored to add stats module

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+from stats import get_num_words
+
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
 
-def count_words(contents):
-    words = contents.split()
-    return words
-
-
 def main():
     file_contents = get_book_text("./books/frankenstein.txt")
-    words = count_words(file_contents)
-    num_words = len(words)
+    num_words = get_num_words(file_contents)
     print(f"Found {num_words} total words")
 
 
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(contents):
+    words = contents.split()
+    return len(words)
