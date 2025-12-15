77a174036a2ca408e0ed16bcfd195d42639a8240
Jeremy Grecu <jeremy.grecu@sap.com>
2025-12-15 17:36:58 +0100

    Refactored, take the book path from argv path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from webbrowser import get
+import sys
 from stats import get_num_words, get_chars, get_chars_list
 
 
@@ -9,7 +9,12 @@ def get_book_text(path_to_file):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    
     file_contents = get_book_text(book_path)
     num_words = get_num_words(file_contents)
     chars_dict = get_chars(file_contents)
